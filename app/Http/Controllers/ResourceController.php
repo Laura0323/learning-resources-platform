dfb07744c38335722911d911008da105bcec5496
Laura0323 <96506699+Laura0323@users.noreply.github.com>
2023-12-14 21:04:07 -0500

    Create and modified resource controller

---
+++ app/Http/Controllers/ResourceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resource;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\Route;
+
+class ResourceController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('Resources', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'resources' => Resource::with('category')->get(),
+        ]);
+    }
+}
